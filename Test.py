884541b1b3f58f85165dc0f281d1c6bb4fb899bd
Harshith Paladi <87645227+HarshithPaladi@users.noreply.github.com>
2021-07-22 10:08:18 +0530

    Python

--- Test.py
+++ Test.py
@@ -1,3 +1,15 @@
+import random
+
+
+def Test():
+    a = 20
+    b = 35
+    c = random.random()
+    print(c)
+    print(a * b - c)
+
+
 print("Hello World")
 print(True)
 print(1 - 4 + 5 / 2 * 6)
+Test()
